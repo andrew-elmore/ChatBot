9df91c7f7e675456f583e2a707871d01cda6ed18
Andrew Elmore <andrewelmore93@gmail.com>
2020-10-21 16:47:39 -0400

    refactors for one event listner

--- features/work_experience.js
+++ features/work_experience.js
@@ -1,155 +1,60 @@
-let library = { 
-    overview: {
-        payload: {
-            text: 'I worked at Ameriprise and AXA. Which would like to know about?',
-            quick_replies: [
-                {
-                    title: 'Ameriprise',
-                    payload: 'An overview of Ameriprise.'
-                }
-            ]
+let library = {
+    work: {
+        payload: 'work',
+        children: {
+            ameriprise: {
+                payload: 'work ampf'
+            },
+            axa: {
+                payload: 'work axa'
+            }
         },
-        keywords: ['work']
     },
-    ameriprise: {
-
-        overview: {
-            payload: {
-                text: 'I worked as a Associate Financial Advisor for Iron Birch Advisors, part of Ameriprise Financial', 
-                quick_replies: [
-                {
-                    title: 'When did you work there?',
-                    payload: 'When did you work at Ameriprise?'
-                },
-                {
-                    title: 'What did you do there',
-                    payload: 'What did you do for Ameriprise'
-                },
-                {
-                    title: 'Where did you work?',
-                    payload: 'Where did you do for Ameriprise'
-                },
-            ]
+    education: {
+        payload: 'edu',
+        children: {
+            appA: {
+                payload: 'edu aa'
             },
-            keywords: ['ameriprise', 'overview']
-        },
-
-        where: {
-            payload: {
-                text: 'here is some text', 
-                quick_replies: [
-                    {
-                        title: 'When did you work there?',
-                        payload: 'When did you work at Ameriprise?'
-                    },
-                    {
-                        title: 'What did you do there',
-                        payload: 'What did you do for Ameriprise'
-                    },
-                    {
-                        title: 'Where did you work?',
-                        payload: 'Where did you do for Ameriprise'
-                    },
-                ]
-            },
-            keywords: ['ameriprise', 'where']
-        },
-
-        what: {
-            payload: {
-                text: 'here is some text', 
-                quick_replies: [
-                    {
-                        title: 'When did you work there?',
-                        payload: 'When did you work at Ameriprise?'
-                    },
-                    {
-                        title: 'What did you do there',
-                        payload: 'What did you do for Ameriprise'
-                    },
-                    {
-                        title: 'Where did you work?',
-                        payload: 'Where did you do for Ameriprise'
-                    },
-                ]
-            },
-            keywords: ['ameriprise', 'what']
-        },
-
-        when: {
-            payload: {
-                text: 'here is some text', 
-                quick_replies: [
-                    {
-                        title: 'When did you work there?',
-                        payload: 'When did you work at Ameriprise?'
-                    },
-                    {
-                        title: 'What did you do there',
-                        payload: 'What did you do for Ameriprise'
-                    },
-                    {
-                        title: 'Where did you work?',
-                        payload: 'Where did you do for Ameriprise'
-                    },
-                ]
-            },
-            keywords: ['ameriprise', 'when']
+            colby: {
+                payload: 'edu colby'
+            }
         }
-
-    }, 
+    }
 }
 
-let allInMessage = function(message, critereas) {
-    return critereas.every((criterea) => message.text.toLowerCase().search(criterea) != -1)
-}
+// let allInMessage = function(message, critereas) {
+//     return critereas.every((criterea) => message.text.toLowerCase().search(criterea) != -1)
+// }
 
+function searchKeywords(lib, keywords) {
+    let results = []
+    let libEntries = Object.entries(lib)
+    libEntries.forEach(([key, value]) => {
+        if (keywords.includes(key)) {
+            results.push(value.payload)
+            if (value.children) {
+                let childRes = searchKeywords(value.children, keywords)
+                if (childRes.length > 0) {
+                    results = childRes
+                }
+            }
+        }
+    });
+    return results
+}
 
 module.exports = function (controller) {
 
-    controller.hears(async (message) => allInMessage(message, library.overview.keywords), ['message'], async (bot, message) => {
+    controller.on('message', async(bot, message) => {
+        let keywords = message.text.toLowerCase().split(' ')
+        let res = searchKeywords(library, keywords)[0]
         await bot.reply(message, { type: 'typing' });
         setTimeout(async () => {
             await bot.changeContext(message.reference);
-            await bot.reply(message, library.overview.payload)
+            await bot.reply(message, {text: res})
         }, 1000);
-    })
-    
-    
-
-    controller.hears(async (message) => allInMessage(message, library.ameriprise.what.keywords), ['message'], async (bot, message) => {
-        await bot.reply(message, { type: 'typing' });
-        setTimeout(async () => {
-            await bot.changeContext(message.reference);
-            await bot.reply(message, library.ameriprise.what.payload)
-        }, 1000);
-    })
-    
-    
-    controller.hears(async (message) => allInMessage(message, library.ameriprise.when.keywords), ['message'], async (bot, message) => {
-        await bot.reply(message, { type: 'typing' });
-        setTimeout(async () => {
-            await bot.changeContext(message.reference);
-            await bot.reply(message, library.ameriprise.when.payload)
-        }, 1000);
-    })
-
-    controller.hears(async (message) => allInMessage(message, library.ameriprise.where.keywords), ['message'], async (bot, message) => {
-        await bot.reply(message, { type: 'typing' });
-        setTimeout(async () => {
-            await bot.changeContext(message.reference);
-            await bot.reply(message, library.ameriprise.where.payload)
-        }, 1000);
-    })
-
-    controller.hears(async (message) => allInMessage(message, library.ameriprise.overview.keywords), ['message'], async (bot, message) => {
-        await bot.reply(message, { type: 'typing' });
-        setTimeout(async () => {
-            await bot.changeContext(message.reference);
-            await bot.reply(message, library.ameriprise.overview.payload)
-        }, 1000);
-    })
-
+    });
 
 
 }
